f82dba4ba4048d94d036808fa0570a6c2afeb5e9
Andrew Martinez <martinez.s.andrew@gmail.com>
2026-06-23 10:21:31 -0400

    Added a GameHistoryEntry model to better store the history data

---
+++ src/MemoryGame/Models/GameHistoryEntry.cs
@@ -0,0 +1,10 @@
+﻿namespace MemoryGame.Models;
+
+public class GameHistoryEntry
+{
+    public int GameNumber { get; set; }
+
+    public GameDifficulty Difficulty { get; set; }
+
+    public TimeSpan Duration { get; set; }
+}
